871201a741c1b3b57a21562fe476a1fe4377b74c
yoyo <timotius_albert@yahoo.com>
2022-02-21 18:00:37 +0700

    updated

--- src/App.js
+++ src/App.js
@@ -123,6 +123,48 @@ function App() {
     </div>
     </div>
 </div>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<h3>More On Warden Feature</h3>
+<div id="title">
+  <div id="title1">
+  THREAT DETECTION
+
+  </div>
+  <div id="title1">
+  IAM SECURITY
+  </div>
+  <div id="title1">
+  REMEDIATION
+  </div>
+  <div id="title1">
+  CLOUD POSTURE
+  </div>
+  <div id="title1">
+  DEVSECOPS
+  </div>
+</div>
+<br></br>
+<div id="lorem">
+<p id="lorem1">Lorem ipsum dolor sit amet</p>
+<p >Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<br></br>
+<p id="lorem1">Sed ut perspiciatis unde omnis</p>
+<p>Iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
+inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
+eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+<br></br>
+<p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea
+commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae
+consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+</div>
     </div>
   );
 }
